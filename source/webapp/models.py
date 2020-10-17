fe3f1e07c54cba0f4c2ebfbddcf7e2970dbbff87
Kurmanbek <kkmambetov@gmail.com>
2020-10-17 13:06:04 +0600

    Добавлен в модель комменты графа like_count

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -52,6 +52,7 @@ class Comment(models.Model):
                                related_name='comments', verbose_name='Автор')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    like_count = models.IntegerField(verbose_name='Счетчик лайков', default=0)
 
     def __str__(self):
         return self.text[:20]
